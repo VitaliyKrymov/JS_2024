b1d0b0cf145378af669f0b33d55e56250d6df207
ViyMix <krymvitaliy@gmail.com>
2024-02-22 21:06:31 +0200

    Add hw1

---
+++ konsultations/konsLes1/main.js
@@ -0,0 +1,17 @@
+console.log('jfkdhs');
+let num='43,752493'
+console.log(num);
+console.log(typeof num);
+console.log(typeof parseInt(num));
+console.log(typeof Number(num));
+
+let name='Vasia';
+let surName='Statham';
+console.log(name+' '+surName);
+console.log(`My name is ${name},my ${surName}`);
+let prm=prompt('Enter your name','Petia');
+
+let age=+prompt('Enter your name','25');
+console.log(age);
+console.log(`name: ${prm}, age: ${age}`);
+console.log(typeof age);
