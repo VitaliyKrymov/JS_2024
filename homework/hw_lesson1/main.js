96220833e116e23c585d503e859614d7dffa38c2
ViyMix <krymvitaliy@gmail.com>
2024-03-10 22:02:18 +0200

    hw1 add

--- homework/hw_lesson1/main.js
+++ homework/hw_lesson1/main.js
@@ -5,26 +5,54 @@ let hello = 'hello';
 console.log(hello);
 let owu = 'owu';
 console.log(owu);
-let com='com';
+let com = 'com';
 console.log(com);
 
-let ua='ua';
-let v1=1;
-let v2=10;
-let v3=-999;
-let v4=123;
-let v5=3.14;
-let v6=2.7;
-let v7=16;
+let v1 = 1;
+console.log(v1);
+let ua = 'ua';
+console.log(ua);
+let v4 = 123;
+console.log(v4);
+let v2 = 10;
+console.log(v2);
+let v3 = -999;
+console.log(v3);
+let v5 = 3.14;
+console.log(v5);
+let v6 = 2.7;
+console.log(v6);
+let v7 = 16;
+console.log(v7);
 
+const status1 = false;
+console.log(status1);
 
-
+const status2 = true;
+console.log(status2);
 
 
 //     - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+const firstName = 'Viy';
+const middleName = 'Mix';
+const lastName = 'Krym';
+let fullName = `${firstName} ${middleName} ${lastName}`;
+console.log(fullName);
 //
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 //     let a = 100; let b = '100'; let c = true;
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
 //
 // Додаткове для тих хто цікавився prompt`oм
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let firstNameFromPromt = prompt('Enter your name','kokos');
+let middleNameFromPromt = prompt('Enter your middle name', 'kokosovich');
+let lastNameFromPromt = prompt('Enter your last name', 'kokoseev');
+
+console.log(firstNameFromPromt+' '+middleNameFromPromt+' '+lastNameFromPromt);
